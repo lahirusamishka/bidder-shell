bdeaf788d1bc4b335bac7d583fc5ec0184e7ce79
Lahiru Samishka <lahirusamishka@gmail.com>
2023-09-15 10:44:26 +0530

    v1.0.0

--- Makefile
+++ Makefile
@@ -4,8 +4,9 @@ down:
 shell:
 	docker-compose -f ../bidder-app/drop-in-docker-php/docker-compose.yml exec -u ${UID}:${UID} app sh
 up:
-	./bidderman.sh
+	./bidderman.sh app_config
 	docker-compose -f ../bidder-app/drop-in-docker-php/docker-compose.yml up -d
 	docker-compose -f ../bidder-app-mysql/docker-compose.yml up -d
+	./bidderman.sh
 up-f:
 	docker-compose -f ../bidder-app/drop-in-docker-php/docker-compose.yml up --build --remove-orphans
--- bidderman.sh
+++ bidderman.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
-LIPA=$(ifconfig | grep -oE 'inet (addr:)?([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v '127.0.0.1' | tail -n 1)
+APP_PARAM=$1
 
-ENV_FILE="../bidder-app/drop-in-docker-php/env/app.env";
+config_function(){
+    LIPA=$(ifconfig | grep -oE 'inet (addr:)?([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v '127.0.0.1' | tail -n 1)
+    ENV_FILE="../bidder-app/drop-in-docker-php/env/app.env";
+    awk -v new_dbhost="$LIPA" -F '=' '$1=="DB_HOST" {$2=new_dbhost} $1=="WEB_SOCKET_HOST" {$2="ws://"new_dbhost":8090/"} $1=="WEB_SOCKET_API" {$2=new_dbhost":8090/"} 1' OFS='=' "$ENV_FILE" > temp && mv temp "$ENV_FILE"
+    echo "successfully changed the ip address on app.env ip address:$LIPA"
+}
 
-awk -v new_dbhost="$LIPA" -F '=' '$1=="DB_HOST" {$2=new_dbhost} $1=="WEB_SOCKET_HOST" {$2="ws://"new_dbhost":8090/"} $1=="WEB_SOCKET_API" {$2=new_dbhost":8090/"} 1' OFS='=' "$ENV_FILE" > temp && mv temp "$ENV_FILE"
+openBrowser(){
+    open "http://localhost:8000/"
+    cd ../bidder-app/
+    code .
+}
 
-cd ../bidder-app/
+open_app(){
+    case "$OSTYPE" in
+        solaris*) echo "SOLARIS" ;;
+        darwin*)  openBrowser ;; 
+        linux*)   openBrowser ;;
+        bsd*)     echo "BSD" ;;
+        msys*)    echo "WINDOWS" ;;
+        cygwin*)  echo "ALSO WINDOWS" ;;
+        *)        echo "unknown: $OSTYPE" ;;
+    esac
+}
 
-code .
+case "$APP_PARAM" in
+   "app_config") config_function 
+   ;;
+   "") open_app 
+   ;;
+esac
+
